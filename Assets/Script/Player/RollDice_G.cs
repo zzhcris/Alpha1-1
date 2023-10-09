5a2e6b4ac658d25a3f78b953a5135dd095761db4
yanjungu-usc <yanjungu@usc.edu>
2023-10-08 21:28:25 -0700

    add-control/reset-on-player-actions-and-enemy-behaviour

--- Assets/Script/Player/RollDice_G.cs
+++ Assets/Script/Player/RollDice_G.cs
@@ -53,47 +53,130 @@ public class RollDice_G : MonoBehaviour
 
 public class Player_G : MonoBehaviour
 {
-    // Define player attributes
     private float speed = 5.0f;
     private float jumpForce = 5.0f;
-    private bool isShielded = false;
     private bool isInvisible = false;
+    private bool isShielded = false;
     private float normalDamage = 10f;
     private float damage = 10f;
+    private float health = 100f;
+    private Renderer rend;
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+    private float gravity = 9.8f;
+
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Basic movement
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(moveX, 0, moveZ) * speed;
+
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // Attack logic
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 1.5f))
+            {
+                var enemy = hit.collider.GetComponent<Enemy_G>();
+                if (enemy)
+                {
+                    enemy.TakeDamage(damage);
+                }
+            }
+        }
+    }
 
     public void GainAbility(string ability)
     {
+        StopAllCoroutines(); // Stop ongoing effects
         switch (ability)
         {
             case "SpeedBoost":
-                speed *= 1.5f; // Increase speed by 50%
+                speed *= 1.5f;
+                StartCoroutine(ResetAbilityAfterSeconds("Speed", 10f)); // Ability lasts for 10 seconds
                 break;
-
             case "JumpBoost":
-                jumpForce *= 1.5f; // Increase jump force by 50%
+                jumpForce *= 1.5f;
+                StartCoroutine(ResetAbilityAfterSeconds("Jump", 10f));
                 break;
-
             case "Invisibility":
-                isInvisible = true;
-                // Implement invisibility effect, e.g., change material transparency
-                GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.5f);
+                rend.material.color = new Color(1, 1, 1, 0.5f); // 50% transparent
                 break;
-
             case "Shield":
                 isShielded = true;
-                // Here, you can add a visual shield or other effects
                 break;
-
             case "DoubleDamage":
-                damage = normalDamage * 2; // Double the damage
+                damage *= 2;
+                StartCoroutine(ResetAbilityAfterSeconds("Damage", 10f));
                 break;
-
             case "Heal":
-                // Assuming there's a health attribute
-                // health += 10; 
-                // Limit the maximum health value, e.g.:
-                // health = Mathf.Min(health, maxHealth);
+                health = Mathf.Min(health + 20, 100); // Assuming max health is 100
                 break;
+        }
+    }
+
+    private System.Collections.IEnumerator ResetAbilityAfterSeconds(string ability, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        switch (ability)
+        {
+            case "Speed":
+                speed /= 1.5f;
+                break;
+            case "Jump":
+                jumpForce /= 1.5f;
+                break;
+            case "Damage":
+                damage = normalDamage;
+                break;
+        }
+    }
+}
+
+public class Enemy_G : MonoBehaviour
+{
+    public float health = 50;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        // Perhaps play a death animation or spawn a pickup item
+    }
+
+    private void Update()
+    {
+        // Simple AI: Follow the player
+        var player = FindObjectOfType<Player>();
+        if (player)
+        {
+            var direction = (player.transform.position - transform.position).normalized;
+            transform.position += direction * 3f * Time.deltaTime; // Assuming enemy speed is 3
         }
     }
 }
